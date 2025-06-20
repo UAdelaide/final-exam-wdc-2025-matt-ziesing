c9a597b1913ab43bd068310a1713205519744ef2
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 03:07:31 +0000

    [automatic commit] 2025-06-20 03:07:30 UTC 193.115.79.194

--- part1/app.js
+++ part1/app.js
@@ -80,7 +80,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [walkersSummary] = await db.execute(`SELECT name, COUNT(walker_id), AVG(rating), COUNT(status)
+    const [walkersSummary] = await db.execute(`SELECT DISTINCT name, COUNT(walker_id), AVG(rating), COUNT(status)
                                                 FROM WalkRatings rate JOIN Users u ON rate.walker_id = u.user_id
                                                 JOIN `);
     res.json(walkersSummary);

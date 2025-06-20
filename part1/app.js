b321bd812821ec8df4e20aebebba8008d68719ca
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 02:47:16 +0000

    [automatic commit] 2025-06-20 02:47:15 UTC 193.115.79.194

--- part1/app.js
+++ part1/app.js
@@ -70,8 +70,8 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [dogList] = await db.execute(`SELECT request_id, name, requested_time, duration_minutes, location,
-                                        'username FROM WalkRequests w JOIN Dogs d ON w.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id',
-                                        'WHERE w.status = "open"`);
+                                        username FROM WalkRequests w JOIN Dogs d ON w.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id
+                                        WHERE w.status = "open"`);
     res.json(dogList);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Open Requests' });

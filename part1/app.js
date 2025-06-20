b5f9fc24f0733eee9fac9594a27b4051fa56cc79
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 01:52:40 +0000

    [automatic commit] 2025-06-20 01:52:39 UTC 193.115.79.194

--- part1/app.js
+++ part1/app.js
@@ -16,7 +16,7 @@ async () => {
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS ');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database

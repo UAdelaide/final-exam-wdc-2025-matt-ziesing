deea7bcab5706a03c58affbeb2166ba49fc87c35
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 02:11:29 +0000

    [automatic commit] 2025-06-20 02:11:29 UTC 193.115.79.194

--- part1/app.js
+++ part1/app.js
@@ -7,57 +7,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mysql = require('mysql');
 
-let db;
-
-(async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root'
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'DogWalkService'
-    });
-
-    app.use(function(req, res, next) {
-    req.pool = dbConnectionPool;
-    next();
-});
-
-    // Insert data if table is empty
-    const [WalkApps] = await db.execute('SELECT COUNT(*) AS count FROM WalkApplications');
-    if (WalkApps[0].count === 0) {
-      await db.execute(`
-        INSERT INTO WalkApplications (request_id, walker_id, applied_at, status) VALUES
-        (1, 2, '2025-04-19 11:30:00, 'accepted'),
-        (3, 4, '2025-06-19 11:30:00', 'rejected'),
-        (2, 5, '2025-06-11 11:30:00', 'pending')
-      `);
-    }
-
-    const [WalkRatings] = await db.execute('SELECT COUNT(*) AS count FROM WalkRatings');
-    if (WalkRatings[0].count === 0) {
-      await db.execute(`
-        INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at) VALUES
-        (1, 2, 3, 5, 'Great Walker. Dog very happy.', '2025-06-10 13:00:00'),
-        (3, 4, 2, 5, 'Really happy with their service', '2025-06-11 12:00:00'),
-        (2, 5, 1, 2, 'Why is my dog covered in mud again?', '2025-05-10 16:00:00')
-      `);
-    }
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
-
 var app = express();
 
 app.use(logger('dev'));

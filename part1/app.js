5df50c5024037e4dc4f609175f709ae491a151d5
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 01:52:19 +0000

    [automatic commit] 2025-06-20 01:52:18 UTC 193.115.79.194

--- part1/app.js
+++ part1/app.js
@@ -13,11 +13,10 @@ async () => {
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS ');
     await connection.end();
 
     // Now connect to the created database

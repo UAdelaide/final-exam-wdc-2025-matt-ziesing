7cb9be8d6439f9a73a131cb1eccf945e6ecd2ac3
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 16:05:20 +0000

    [automatic commit] 2025-06-20 16:05:20 UTC 193.115.107.245

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -55,9 +55,9 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
-    res.json({ message: 'Login successful', user: rows[0] });
+    
     req.session.userID = rows[0].user_id;
+    res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }

4c236bea00da22e410c379980ab507147f9cb4be
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 04:12:41 +0000

    [automatic commit] 2025-06-20 04:12:40 UTC 193.115.79.194

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,7 +11,8 @@ router.get('/dogPic', async(req, res, next) => {
 
   try {
     let response = await fetch('https://dog.ceo/api/breeds/image/random');
-    
+    let data = await response.json();
+    res.json()
   } catch (error) {
     res.status(500).json( { error: 'error fetching image' });
   }

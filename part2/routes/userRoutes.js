6092769f81606d359efe13df6aa82bb90b372a50
Matthew Ziesing <mattzinger852@gmail.com>
2025-06-20 23:17:22 +0000

    [automatic commit] 2025-06-20 23:17:22 UTC 193.115.107.245

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -73,6 +73,9 @@ router.get('/allDogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
       SELECT * from Dogs`);
+      res.json(rows);
+  } catch (error) {
+    res.status(500).json
   }
 })
 
